f3c75d8d97dd397e95d0c92bc2cd1aff1197d436
lubayb1 <bryanlubay1@gmail.com>
2020-05-24 18:00:19 -0700

    24 may

---
+++ bryanlubay/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  devServer: {
+    contentBase: __dirname + "/public",
+    inline: true,
+    host: '0.0.0.0',
+    port: 8080,
+  },
+  output:{
+    path: __dirname + "/src",
+    filename: "App.js",
+    chunkFilename: '[name].js'
+
+  }
+}
